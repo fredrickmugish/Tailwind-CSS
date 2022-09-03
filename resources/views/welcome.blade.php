beea9958fbe16b618f113b69bdbf0424b3089cae
fredrickmugish <fredrickgotfried@gmail.com>
2022-09-03 12:06:06 -0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MyPortifolio</title>
+        @vite('resources/js/app.js')
+       
+      
+    </head>
+    <body class="antialiased">
+        
+        <div class="flex justify-center items-center h-screen">
+            <h1 class="text-3xl text-purple-600 font-bold">Hi! My Name is Fredrick Mugisha I welcome you to my portifolio website</h1>
+        </div>
+
+
+
+    </body>
+</html>
